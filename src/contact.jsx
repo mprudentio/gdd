2d08a0c996f3b3236ec1587efdb083a17c818674
mprudentio <muhammad.prudentio@ui.ac.id>
2023-09-21 17:08:10 +0700

    add new project

--- src/contact.jsx
+++ src/contact.jsx
@@ -5,12 +5,12 @@ const Contact = () => {
     <div name='contact' className='pt-10 w-full h-screen bg-[#0a192f] flex justify-center items-center px-4 pb-4 '>
       <form method='POST' action="https://getform.io/f/becf52d1-cc01-4324-bf6f-5ea1529dc8a3" className='flex flex-col w-full pt-16 max-w-[600px]'>
         <div className='pb-8'>
-         <p  className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
+          <p  className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
           <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - muprudentio@gmail.com</p>
         </div>
-        <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-        <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-        <textarea className='bg-[#ccd6f6] p-2 ' placeholder='Message' name="message" id="" cols="30" rows="10"></textarea>
+        <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' required/>
+        <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' required/>
+        <textarea className='bg-[#ccd6f6] p-2 ' placeholder='Message' name="message" id="" cols="30" rows="10" required></textarea>
         <button className='text-white bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded-full px-4 py-1 my-8 mx-auto flex items-center'>Let's Collaborate</button>
       </form>
     </div>
--- src/work.jsx
+++ src/work.jsx
@@ -1,30 +1,122 @@
-import React from 'react'
+import React from 'react';
 
 const Work = () => {
   return (
-    <div name='work' className='w-full bg-[#0a192f] md:h-screen text-gray-300'>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div  className='pb-8'>
-          <p className='pt-7 md:t-4 text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
-          <p className='py-6'>Check out of my recent work</p>
+        <div className='pb-8 md:mt-3 lg:mt-1'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+            Work
+          </p>
+          <p className='py-6'>// Check out some of my recent work</p>
         </div>
 
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          <div style={{ backgroundImage: `url('./cyber-bg.png')` }}className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white  tracking-wider'>Clouder</span>
+{/* Container */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-4 gap-4'>
 
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url('./cyber-bg.png')` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div text-center'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Store Dashboard
+              </span>
               <div className='pt-8 text-center'>
-                <a href="https://super-cool-site-by-muprudentio-gmail-com-b8520.netlify.app/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Visit</button>
+                {/* <a href='https://super-cool-site-by-muprudentio-gmail-com-b8520.netlify.app/' target={"_blank"}>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a> */}
+                <a href='https://store-dashboard-beta-mprudentio.vercel.app/' target={"_blank"}>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/mprudentio/store-dashboard' target={"_blank"}>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
                 </a>
               </div>
             </div>
           </div>
+          <div
+            style={{ backgroundImage: `url('./library.png')` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div text-center'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Bookshelf
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://amazing-crumble-f8d7e2.netlify.app/' target={"_blank"}>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/mprudentio/bookshelf' target={"_blank"} >
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url('./weather.png')` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div text-center'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Weather
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://aquamarine-nasturtium-312cfc.netlify.app/' target={"_blank"}>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/mprudentio/weather-app' target={"_blank"}>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url('./ecommerce.png')`}}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div text-center'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                E-Commerce
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://ecommerce-f6gj.vercel.app/' target={"_blank"}>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://gitlab.com/mprudentio/ecommerce' target={"_blank"}>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          </div>
+          </div>
         </div>
-      </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Work;
