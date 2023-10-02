f5352173852ec6c0d2dfc97b307799a31845751e
mprudentio <muhammad.prudentio@ui.ac.id>
2023-10-03 06:02:59 +0700

    fix navbar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -16,26 +16,26 @@ function Navbar() {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 '>
     <div>
-        <img src="./Logo.png" alt="Logo" style={{width:'50px'}}/>
+      <Link to='home' smooth={true} duration={500} className='cursor-pointer'><img src="./Logo.png" alt="Logo" style={{width:'50px'}}/> </Link>
     </div>
 
     {/* Menu */}
         <ul className='hidden md:flex'>
-            <li className='hover:border-b-2 hover:border-pink-600'><Link to='home' smooth={true} duration={500}>
+          <Link to='home' smooth={true} duration={500}>  <li className='hover:border-b-2 hover:border-pink-600'>
             Home
-          </Link></li>
-            <li className='hover:border-b-2 hover:border-pink-600'><Link to='about' smooth={true} duration={500}>
+          </li></Link>
+          <Link to='about' smooth={true} duration={500}>  <li className='hover:border-b-2 hover:border-pink-600'>
             About
-          </Link></li>
-            <li className='hover:border-b-2 hover:border-pink-600'><Link to='skills' smooth={true} duration={500}>
+          </li></Link>
+          <Link to='skills' smooth={true} duration={500}>  <li className='hover:border-b-2 hover:border-pink-600'>
             Skills
-          </Link></li>
-            <li className='hover:border-b-2 hover:border-pink-600'><Link to='work' smooth={true} duration={500}>
+          </li></Link>
+          <Link to='work' smooth={true} duration={500}> <li className='hover:border-b-2 hover:border-pink-600'>
             Work
-          </Link></li>
-            <li className='hover:border-b-2 hover:border-pink-600'><Link to='contact' smooth={true} duration={500}>
+          </li> </Link>
+          <Link to='contact' smooth={true} duration={500}><li className='hover:border-b-2 hover:border-pink-600'>
             Contact
-          </Link></li>
+         </li> </Link>
         </ul>
 
 
@@ -50,21 +50,21 @@ function Navbar() {
             ? 'hidden'
             : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
         } >
-            <li className='py-4 text-3xl'><Link onClick={handleClick} to='home' smooth={true} duration={500}>
+           <Link onClick={handleClick} to='home' smooth={true} duration={500}> <li className='py-4 text-3xl'>
             Home
-          </Link></li>
-            <li className='py-4 text-3xl'><Link onClick={handleClick} to='about' smooth={true} duration={500}>
+          </li></Link>
+          <Link onClick={handleClick} to='about' smooth={true} duration={500}> <li className='py-4 text-3xl'>
             About
-          </Link></li>
-            <li className='py-4 text-3xl'><Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+          </li></Link>
+          <Link onClick={handleClick} to='skills' smooth={true} duration={500}> <li className='py-4 text-3xl'>
             Skills
-          </Link></li>
-            <li className='py-4 text-3xl'><Link onClick={handleClick} to='work' smooth={true} duration={500}>
+          </li></Link>
+          <Link onClick={handleClick} to='work' smooth={true} duration={500}><li className='py-4 text-3xl'>
             Work
-          </Link></li>
-            <li className='py-4 text-3xl'><Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+          </li></Link>
+          <Link onClick={handleClick} to='contact' smooth={true} duration={500}><li className='py-4 text-3xl'>
             Contact
-          </Link></li>
+          </li></Link>
         </ul>
 
     {/* Social Icons */}
